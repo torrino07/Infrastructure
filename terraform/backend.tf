311e4d3f474f5cd4c00ec0fedb1324767ca00d17
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-09-30 13:34:22 +0200

    test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket         = "terraformstate001"
-        key            = "terraform.tfstate"
+        key            = "${terraform.workspace}/terraform.tfstate"
         region         = "us-east-1"
         dynamodb_table = "terraform-locks"
         encrypt        = true
