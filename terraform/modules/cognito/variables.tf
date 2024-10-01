f3aaa548884ba1f7a9903648a3d08b9d9c39e37c
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-01 12:08:14 +0200

    added cognito

---
+++ terraform/modules/cognito/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "The environment (dev, prod, test)"
+  type        = string
+}
+
+
+variable "name" {
+  description = "Name of the cognito object"
+  type        = string
+}
