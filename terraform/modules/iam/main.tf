0c78496951572fb89c259841cd59c9b8b6e23eaa
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-13 20:57:19 +0200

    tesrt

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,12 +1,12 @@
 resource "aws_iam_role" "this" {
   name = "${var.environment}-${var.name}-iam-role"
-  assume_role_policy = file("${var.assume_role_policy_path}")
+  assume_role_policy = file("${path.module}/${var.assume_role_policy_path}")
 }
 
 resource "aws_iam_role_policy" "this" {
   name   =  "${var.environment}-${var.name}-iam-role-policy"
   role   = aws_iam_role.this.id
-  policy = file("${var.policy_path}")
+  policy = file("${path.module}/${var.policy_path}")
 }
 
 resource "aws_iam_instance_profile" "this" {
