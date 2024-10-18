b87e961f3308c9118f24dae38027a0c46123ea95
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-18 11:56:51 +0200

    test

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "this" {
-  private_key       = file("${var.private_key}")
-  certificate_body  = file("${var.private_body}")
-  certificate_chain = file("${var.private_chain}")
+  private_key       = var.private_key
+  certificate_body  = var.private_body
+  certificate_chain = var.private_chain
 }
