4eac253ec9ba46b9fbf04d830b4bdfcd4d71e5a6
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-19 20:45:43 +0200

    test

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,20 +1,18 @@
 resource "aws_eks_cluster" "this" {
-  for_each   = toset(var.subnet_ids)
   name     = "${var.environment}-${var.cluster_name}"
   role_arn = var.eks_cluster_role_arn
 
   vpc_config {
-    subnet_ids         = [each.value]
+    subnet_ids         = var.subnet_ids
     security_group_ids = [var.sg_id]
   }
 }
 
 resource "aws_eks_node_group" "this" {
-  for_each   = toset(var.subnet_ids)
-  cluster_name    = aws_eks_cluster.this.name
+  cluster_name    = "${var.environment}-${var.cluster_name}"
   node_group_name = "${var.environment}-${var.node_group_name}"
   node_role_arn   = var.eks_node_role_arn
-  subnet_ids      = [each.value]
+  subnet_ids      = var.subnet_ids
 
   scaling_config {
     desired_size = var.desired_capacity
--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,14 +1,4 @@
-output "eks_cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = aws_eks_cluster.this.name
-}
-
 output "eks_cluster_endpoint" {
   description = "The endpoint of the EKS cluster"
   value       = aws_eks_cluster.this.endpoint
-}
-
-output "eks_node_group_name" {
-  description = "The name of the node group"
-  value       = aws_eks_node_group.this.node_group_name
 }
