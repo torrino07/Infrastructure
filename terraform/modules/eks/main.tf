6e55352f61a0e53c5115988c856b86e128b2e9da
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-25 20:17:10 +0200

    test

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -5,13 +5,14 @@ module "eks" {
   cluster_name    = "${var.environment}-${var.cluster_name}"
   cluster_version = "1.31"
 
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  # cluster_endpoint_private_access = true
 
   vpc_id                   = var.vpc_id
   subnet_ids               = var.subnet_ids
   control_plane_subnet_ids = var.subnet_ids
   enable_irsa              = true
+  
 
   cluster_addons = {
     coredns = {
@@ -26,14 +27,15 @@ module "eks" {
   }
 
   eks_managed_node_group_defaults = {
-    ami_type               = "AL2_x86_64"
     instance_types         = [var.instance_type]
-    vpc_security_group_ids = [var.sg_id]
   }
 
   eks_managed_node_groups = {
     node_group = {
+      ami_type             = "AL2023_x86_64_STANDARD"
+      instance_types       = [var.instance_type]
       capacity_type        = "ON_DEMAND"
+      
       name                 = "${var.environment}-general"
       desired_size         = var.desired_capacity
       max_size             = var.max_size
