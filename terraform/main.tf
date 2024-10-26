8ab51a07836ddce6f7d772bc4a69dfe7bd1e82fe
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-26 09:54:49 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,91 +1,82 @@
-provider "aws" {
-  region = local.region
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
 
-locals {
-  name   = "ascode-cluster"
-  region = "us-east-1"
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
 
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
 
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 
-  tags = {
-    Example = local.name
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
   }
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/20"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
 
-  name = local.name
-  cidr = local.vpc_cidr
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.16.0/20"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
-
-  enable_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
+resource "aws_subnet" "subnet_3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.32.0/20"
+  availability_zone       = "us-east-1d"
+  map_public_ip_on_launch = true
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "~> 19.0"
 
-  cluster_name                   = local.name
+  cluster_name    = "my-cluster-eks"
+  cluster_version = "1.27"
+
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
+  vpc_id                   = aws_vpc.main.id
+  subnet_ids               = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+  control_plane_subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
 
   eks_managed_node_groups = {
-    ascode-cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
+    green = {
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      instance_types = ["t3.medium"]
     }
   }
+}
 
-  tags = local.tags
+resource "aws_ecr_repository" "microservice_a" {
+  name = "microservice-a"
+}
+
+resource "aws_ecr_repository" "microservice_b" {
+  name = "microservice-b"
+}
+
+output "ecr_repository_url_microservice_a" {
+  value = aws_ecr_repository.microservice_a.repository_url
+}
+
+output "ecr_repository_url_microservice_b" {
+  value = aws_ecr_repository.microservice_b.repository_url
 }
