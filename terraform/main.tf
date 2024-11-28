e456a1f333dec52dfe4bd45864d3b6a2eeb7a2d4
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-11-28 18:29:52 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -283,7 +283,6 @@ module "iam" {
         "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
         "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
         "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-        "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
       ],
       access_level = "readwrite"
     },
