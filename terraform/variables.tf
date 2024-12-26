012fbf0f41e3c61e995b404002b112c976a065ba
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-12-26 09:35:39 +0100

    test

---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+variable "account_id" {
+  default = "160945804984"
+}
+
+variable "proj" {
+  default = "tradingbot"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "region" {
+  default = "us-east"
+}
+
+variable "artifact_store" {
+  default = "artifactstore001"
+}
+
+variable "availability_zone_ebs" {
+  default = "us-east-1a"
+}
+
+variable "ec2_instance_type" {
+  default = "t4g.medium"
+}
+
+variable "ec2_ami_type" {
+  default = "ami-07ee04759daf109de"
+}
+
+variable "eks_version" {
+  default = "1.31"
+}
+
+
