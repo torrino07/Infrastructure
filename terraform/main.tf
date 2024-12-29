8464a08c8bd8f8011e214f0e159371fc3664fb74
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-12-29 22:31:00 +0100

    🔧 test

--- terraform/main.tf
+++ terraform/main.tf
@@ -355,62 +355,62 @@ module "iam" {
   ]
 }
 
-# module "s3" {
-#   depends_on  = [module.endpoints]
-#   source      = "./modules/s3"
-#   proj        = var.proj
-#   environment = var.environment
-#   bucket      = "${var.artifact_store}-${var.environment}"
-#   policy = {
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "s3:PutObject",
-#           "s3:GetObject",
-#           "s3:ListBucket"
-#         ]
-#         Principal = "*"
-#         Resource = [
-#           "arn:aws:s3:::${var.artifact_store}-${var.environment}",
-#           "arn:aws:s3:::${var.artifact_store}-${var.environment}/*"
-#         ]
-#         Condition = {
-#           StringEquals = {
-#             "aws:SourceVpce" = module.endpoints.ids["tradingbot-${var.environment}-s3"]
-#           }
-#         }
-#       }
-#     ]
-#   }
-# }
+module "s3" {
+  depends_on  = [module.endpoints]
+  source      = "./modules/s3"
+  proj        = var.proj
+  environment = var.environment
+  bucket      = "${var.artifact_store}-${var.environment}"
+  policy = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Principal = "*"
+        Resource = [
+          "arn:aws:s3:::${var.artifact_store}-${var.environment}",
+          "arn:aws:s3:::${var.artifact_store}-${var.environment}/*"
+        ]
+        Condition = {
+          StringEquals = {
+            "aws:SourceVpce" = module.endpoints.ids["tradingbot-${var.environment}-s3"]
+          }
+        }
+      }
+    ]
+  }
+}
 
-############ EC2 ############
-# module "ec2" {
-#   depends_on    = [module.iam]
-#   source        = "./modules/ec2"
-#   proj          = var.proj
-#   environment   = var.environment
-#   name          = "trading-server"
-#   subnet_id     = module.subnets.ids["tradingbot-${var.environment}-ec2-private-1c-1"]
-#   sg_id         = module.sg.ids["tradingbot-${var.environment}-ec2-sg"]
-#   instance_type = var.ec2_instance_type
-#   ami           = var.ec2_ami_type
-#   role_arn_name = "AmazonEC2Role"
-#   access_level  = "readwrite"
-# }
+########### EC2 ############
+module "ec2" {
+  depends_on    = [module.iam]
+  source        = "./modules/ec2"
+  proj          = var.proj
+  environment   = var.environment
+  name          = "trading-server"
+  subnet_id     = module.subnets.ids["tradingbot-${var.environment}-ec2-private-1c-1"]
+  sg_id         = module.sg.ids["tradingbot-${var.environment}-ec2-sg"]
+  instance_type = var.ec2_instance_type
+  ami           = var.ec2_ami_type
+  role_arn_name = "AmazonEC2Role"
+  access_level  = "readwrite"
+}
 
-########### EBS ############
-# module "ebs" {
-#   depends_on        = [module.iam]
-#   source            = "./modules/ebs"
-#   proj              = var.proj
-#   environment       = var.environment
-#   ebs_volume_size   = 20
-#   ebs_volume_type   = "gp3"
-#   availability_zone = "${var.region}a"
-# }
+########## EBS ############
+module "ebs" {
+  depends_on        = [module.iam]
+  source            = "./modules/ebs"
+  proj              = var.proj
+  environment       = var.environment
+  ebs_volume_size   = 20
+  ebs_volume_type   = "gp3"
+  availability_zone = "${var.region}a"
+}
 
 ########### EKS ############
 module "eks" {
@@ -442,56 +442,56 @@ module "eks" {
   ]
 }
 
-# ###### COGNITO ##########
-# module "cognito" {
-#   source      = "./modules/cognito"
-#   proj        = var.proj
-#   environment = var.environment
-#   name        = "x-turbo"
-# }
+###### COGNITO ##########
+module "cognito" {
+  source      = "./modules/cognito"
+  proj        = var.proj
+  environment = var.environment
+  name        = "x-turbo"
+}
 
-# ######### ECR ##########
-# module "ecr" {
-#   source      = "./modules/ecr"
-#   proj        = var.proj
-#   environment = var.environment
-#   repositories = [
-#     {
-#       name                 = "fastapi-app"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     },
-#     {
-#       name                 = "react-app"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     }
-#     ,
-#     {
-#       name                 = "postgresql-server"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     },
-#     {
-#       name                 = "metrics-scraper"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     },
-#     {
-#       name                 = "dashboard"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     },
-#     {
-#       name                 = "controller"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     },
-#     {
-#       name                 = "kube-webhook-certgen"
-#       scan_on_push         = true
-#       image_tag_mutability = "MUTABLE"
-#     }
-#   ]
-# }
+######### ECR ##########
+module "ecr" {
+  source      = "./modules/ecr"
+  proj        = var.proj
+  environment = var.environment
+  repositories = [
+    {
+      name                 = "fastapi-app"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    },
+    {
+      name                 = "react-app"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    }
+    ,
+    {
+      name                 = "postgresql-server"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    },
+    {
+      name                 = "metrics-scraper"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    },
+    {
+      name                 = "dashboard"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    },
+    {
+      name                 = "controller"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    },
+    {
+      name                 = "kube-webhook-certgen"
+      scan_on_push         = true
+      image_tag_mutability = "MUTABLE"
+    }
+  ]
+}
 
