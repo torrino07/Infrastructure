f33fe3b84826f0087aa5c2183cf8a0d1fc3d6988
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-01-04 14:10:02 +0100

    test

--- terraform/modules/acm/outputs.tf
+++ terraform/modules/acm/outputs.tf
@@ -1,7 +1,7 @@
 output "server_certificate" {
-  value = aws_acm_certificate.server_vpn_cert
+  value = aws_acm_certificate.server_vpn_cert.arn
 }
 
 output "client_certificate" {
-  value = aws_acm_certificate.client_vpn_cert
+  value = aws_acm_certificate.client_vpn_cert.arn
 }
