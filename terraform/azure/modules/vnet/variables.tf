3254b05f1aef7c3ff40867243ff8577dcf294943
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-11-09 11:30:36 +0100

    test

--- terraform/azure/modules/vnet/variables.tf
+++ terraform/azure/modules/vnet/variables.tf
@@ -3,7 +3,7 @@ variable "location" { type = string }
 variable "resource_group_name" { type = string }
 variable "address_space" { type = list(string) }
 variable "tags" { type = map(string) }
-variable "dns_servers" { 
-    type = list(string) 
-    default = []
+variable "dns_servers" {
+  type    = list(string)
+  default = []
 }
