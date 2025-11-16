e865a83f62dd23d33a98de579f7101f6c33e3a85
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-11-16 02:04:55 +0100

    test

--- terraform/azure/modules/nsg/main.tf
+++ terraform/azure/modules/nsg/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_security_group" "this" {
   name                = var.name
-  location            = var.region
+  location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
 
--- terraform/azure/modules/route_tables/variables.tf
+++ terraform/azure/modules/route_tables/variables.tf
@@ -1,5 +1,5 @@
 variable "name" { type = string }
-variable "region" { type = string }
+variable "location" { type = string }
 variable "resource_group_name" { type = string }
 variable "disable_bgp_propagation" { type = bool }
 variable "tags" { type = map(string) }
